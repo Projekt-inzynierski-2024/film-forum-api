c8746999fc999e037a892699587d28a8da052f7d
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-10-29 15:59:40 +0100

    CreateFilmDto fix

--- FilmForumWebAPI/Controllers/FilmController.cs
+++ FilmForumWebAPI/Controllers/FilmController.cs
@@ -1,8 +1,5 @@
-﻿using FilmForumWebAPI.Extensions;
-using FilmForumWebAPI.Models.Dtos.FilmDtos;
+﻿using FilmForumWebAPI.Models.Dtos.FilmDtos;
 using FilmForumWebAPI.Services.Interfaces;
-using FluentValidation;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmForumWebAPI.Controllers;
@@ -12,23 +9,15 @@ namespace FilmForumWebAPI.Controllers;
 public class FilmController : ControllerBase
 {
     private readonly IFilmService _filmService;
-    private readonly IValidator<CreateFilmDto> _createFilmValidator;
 
-    public FilmController(IFilmService filmService, IValidator<CreateFilmDto> createFilmValidator)
+    public FilmController(IFilmService filmService)
     {
         _filmService = filmService;
-        _createFilmValidator = createFilmValidator;
     }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateFilmDto createFilmDto)
     {
-        ValidationResult validation = _createFilmValidator.Validate(createFilmDto);
-        if (!validation.IsValid)
-        {
-            return BadRequest(validation.Errors.GetMessagesAsString());
-        }
-
         await _filmService.CreateAsync(createFilmDto);
         return Ok();
     }
@@ -37,8 +26,8 @@ public class FilmController : ControllerBase
     public async Task<IActionResult> GetAll() => Ok(await _filmService.GetAllAsync());
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(string id) 
-        => await _filmService.GetAsync(id) is GetFilmDto film ? Ok(film) : NotFound($"Film not found"); 
+    public async Task<IActionResult> GetById(string id)
+        => await _filmService.GetAsync(id) is GetFilmDto film ? Ok(film) : NotFound($"Film not found");
 
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(string id, [FromBody] CreateFilmDto updatedFilm)
