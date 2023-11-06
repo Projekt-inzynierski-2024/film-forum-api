f22618b39f9c81eba415961d6f11b6b5c286e4b9
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-11-06 12:55:06 +0100

    Prevent authorization with null user

--- src/AuthenticationManager/Extensions/AuthenticationManagerExtension.cs
+++ src/AuthenticationManager/Extensions/AuthenticationManagerExtension.cs
@@ -4,6 +4,7 @@ using FilmForumModels.Models.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using System.Text;
 
 namespace AuthenticationManager.Extensions;
@@ -32,7 +33,26 @@ public static class AuthenticationManagerExtension
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtDetails.SecretKey)),
                         ValidateIssuerSigningKey = true
                     };
+                    options.Events = new JwtBearerEvents()
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            string? userId = context?.Principal?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+                            string? username = context?.Principal?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value;
+                            string? email = context?.Principal?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+                            //Avoid trying to authorize with user who doesn't exist
+                            if (IsAnyClaimValueNull(new string?[] { userId, username, email }))
+                            {
+                                context?.Fail("Unauthorized. User is null");
+                                return Task.CompletedTask;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
         return services;
     }
+
+    private static bool IsAnyClaimValueNull(IEnumerable<string?> claimsValues) => claimsValues.Any(string.IsNullOrWhiteSpace);
 }
