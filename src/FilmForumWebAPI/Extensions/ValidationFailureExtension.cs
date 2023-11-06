0311b3cf9b3bd559e829e1757f8fbd902c8d23d7
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-11-06 15:42:20 +0100

    Unit tests

--- src/FilmForumWebAPI/Extensions/ValidationFailureExtension.cs
+++ src/FilmForumWebAPI/Extensions/ValidationFailureExtension.cs
@@ -9,6 +9,7 @@ public static class ValidationFailureExtension
     /// </summary>
     /// <param name="validationFailures">Errors of validation</param>
     /// <returns>Errors' messages as string</returns>
+    /// <exception cref="ArgumentNullException">When <paramref name="validationFailures"/> is null</exception>
     public static string GetMessagesAsString(this IEnumerable<ValidationFailure> validationFailures)
         => string.Join(Environment.NewLine, validationFailures.Select(x => x.ErrorMessage));
 }
---
+++ tests/FilmForumWebAPI.UnitTests/Extensions/ValidationFailureExtensionTests.cs
@@ -0,0 +1,51 @@
+﻿using FilmForumWebAPI.Extensions;
+using FluentAssertions;
+using FluentValidation.Results;
+
+namespace FilmForumWebAPI.UnitTests.Extensions;
+
+public class ValidationFailureExtensionTests
+{
+    [Fact]
+    public void GetMessagesAsString_ForEmptyList_ReturnEmptyString()
+    {
+        string NameErrorMessage = "Name was null";
+        string SecondNameErrorMessage = "Second name was null";
+        string EmailErrorMessage = "Email has less than 6 characters";
+
+        List<ValidationFailure> validationFailures = new()
+        {
+            new ValidationFailure("Name", NameErrorMessage),
+            new ValidationFailure("SecondName", SecondNameErrorMessage),
+            new ValidationFailure("Email", EmailErrorMessage)
+        };
+
+        string expected = $"{NameErrorMessage}{Environment.NewLine}{SecondNameErrorMessage}{Environment.NewLine}{EmailErrorMessage}";
+
+        //Act
+        string result = validationFailures.GetMessagesAsString();
+
+        //Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void GetMessagesAsString_ForEmptyFailureList_ReturnsEmptyString()
+    {
+        //Act
+        string result = ValidationFailureExtension.GetMessagesAsString(Enumerable.Empty<ValidationFailure>());
+
+        //Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetMessagesAsString_ForNullArguments_ThrowArgumentNullException()
+    {
+        //Act
+        Func<string> action = () => ValidationFailureExtension.GetMessagesAsString(null!);
+
+        //Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+}
