4a55017c8e1180d3196d6613b14d35e6457d0601
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-11-13 18:49:57 +0100

    RoleController tests

---
+++ src/FilmForumWebAPI/Controllers/RoleController.cs
@@ -0,0 +1,31 @@
+﻿using FilmForumWebAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmForumWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IRoleService _rolesService;
+
+    public RoleController(IRoleService roleService,
+                          IUserService userService)
+    {
+        _rolesService = roleService;
+        _userService = userService;
+    }
+
+    [HttpGet("user-roles-names{userId}")]
+    public async Task<IActionResult> GetUserRolesNames(int userId) 
+        => ! await _userService.UserWithIdExistsAsync(userId)
+           ? NotFound("User not found") 
+           : Ok(await _rolesService.GetUserRolesNamesAsync(userId));
+
+    [HttpGet("user-roles{userId}")]
+    public async Task<IActionResult> GetUserRoles(int userId) 
+        => ! await _userService.UserWithIdExistsAsync(userId) 
+           ? NotFound("User not found") 
+           : Ok(await _rolesService.GetUserRolesAsync(userId));
+}
---
+++ tests/FilmForumWebAPI.UnitTests/Controllers/RoleControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using FilmForumModels.Dtos.RoleDtos;
+using FilmForumWebAPI.Controllers;
+using FilmForumWebAPI.Services.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FilmForumWebAPI.UnitTests.Controllers;
+
+public class RoleControllerTests
+{
+    private readonly RoleController _roleController;
+
+    private readonly Mock<IRoleService> _roleServiceMock = new();
+    private readonly Mock<IUserService> _userServiceMock = new();
+
+    public RoleControllerTests()
+    {
+        _roleController = new(_roleServiceMock.Object,
+                              _userServiceMock.Object);
+    }
+
+    public async Task GetUserRolesNames_ForValidData_ReturnsUserRolesName()
+    {
+        // Arrange
+        List<string> rolesNames = new() { "Admin", "Moderator", "User" };
+        _userServiceMock.Setup(x => x.UserWithIdExistsAsync(It.IsAny<int>())).ReturnsAsync(true);
+        _roleServiceMock.Setup(x => x.GetUserRolesNamesAsync(It.IsAny<int>())).ReturnsAsync(rolesNames);
+
+        // Act
+        IActionResult result = await _roleController.GetUserRolesNames(It.IsAny<int>());
+
+        // Assert
+        OkObjectResult okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okObjectResult.Value.Should().BeEquivalentTo(rolesNames);
+    }
+
+    public async Task GetUserRolesNames_ForNonExistingUser_ReturnsNotFound()
+    {
+        // Arrange
+        List<string> rolesNames = new() { "Admin", "Moderator", "User" };
+        _userServiceMock.Setup(x => x.UserWithIdExistsAsync(It.IsAny<int>())).ReturnsAsync(false);
+
+        // Act
+        IActionResult result = await _roleController.GetUserRolesNames(It.IsAny<int>());
+
+        // Assert
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+    public async Task GetUserRoles_ForValidData_ReturnsUserRoles()
+    {
+        // Arrange
+        List<GetUserRoleDto> roles = new()
+        {
+            new(1, "Admin", DateTime.UtcNow, 1),
+            new(2, "Moderator", DateTime.UtcNow, 1),
+            new(3, "User", DateTime.UtcNow, 1),
+        };
+        _userServiceMock.Setup(x => x.UserWithIdExistsAsync(It.IsAny<int>())).ReturnsAsync(true);
+        _roleServiceMock.Setup(x => x.GetUserRolesAsync(It.IsAny<int>())).ReturnsAsync(roles);
+
+        // Act
+        IActionResult result = await _roleController.GetUserRoles(It.IsAny<int>());
+
+        // Assert
+        OkObjectResult okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okObjectResult.Value.Should().BeEquivalentTo(roles);
+    }
+
+    public async Task GetUserRoles_ForNonExistingUser_ReturnsNotFound()
+    {
+        // Arrange
+        _userServiceMock.Setup(x => x.UserWithIdExistsAsync(It.IsAny<int>())).ReturnsAsync(false);
+
+        // Act
+        IActionResult result = await _roleController.GetUserRoles(It.IsAny<int>());
+
+        // Assert
+        result.Should().BeOfType<NotFoundObjectResult>();
+    }
+}
