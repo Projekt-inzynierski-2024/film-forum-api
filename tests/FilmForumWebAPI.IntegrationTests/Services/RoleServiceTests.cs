0972c541912c77f2e348909aaba1893b644df5d5
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-11-19 00:39:32 +0100

    Integration tests for RoleService

--- tests/FilmForumWebAPI.IntegrationTests/Services/RoleServiceTests.cs
+++ tests/FilmForumWebAPI.IntegrationTests/Services/RoleServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using FilmForumModels.Entities;
+﻿using FilmForumModels.Dtos.RoleDtos;
+using FilmForumModels.Entities;
 using FilmForumWebAPI.Database;
 using FilmForumWebAPI.IntegrationTests.HelpersForTests;
 using FilmForumWebAPI.Services;
@@ -10,13 +11,41 @@ public class RoleServiceTests
 {
     public static IEnumerable<object[]> GetUserRolesNamesAsyncTestsData()
     {
-        yield return new object[] { 1, new List<string>() { "Admin", "Moderator", "User" } };
-        yield return new object[] { 99999, new List<string>() };
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 1 , UserId = 1},
+                new() { RoleId = 2 , UserId = 1},
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<string>() { "Admin", "Moderator", "User" }
+        };
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 2 , UserId = 1},
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<string>() { "Moderator", "User" }
+        };
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<string>() { "User" }
+        };
     }
 
     [Theory]
     [MemberData(nameof(GetUserRolesNamesAsyncTestsData))]
-    public async Task GetUserRolesNamesAsync_ForGivenId_ReturnsUserRolesNamesIfUserExistsOtherwiseReturnsEmptyList(int userId, List<string> expected)
+    public async Task GetUserRolesNamesAsync_ForExistingUser_ReturnsUserRolesNames(int userId, List<UserToRole> userToRoles, List<string> expected)
     {
         //Arrange
         UsersDatabaseContext usersDatabaseContext = await DatabaseHelper.CreateAndPrepareUsersDatabaseContextForTesting();
@@ -28,12 +57,7 @@ public class RoleServiceTests
             new() { Name = "Moderator" },
             new() { Name = "User" },
         });
-        await usersDatabaseContext.UsersToRoles.AddRangeAsync(new List<UserToRole>()
-        {
-            new() { RoleId = 1, UserId = 1 },
-            new() { RoleId = 2, UserId = 1 },
-            new() { RoleId = 3, UserId = 1 },
-        });
+        await usersDatabaseContext.UsersToRoles.AddRangeAsync(userToRoles);
         await usersDatabaseContext.SaveChangesAsync();
 
         //Act
@@ -41,5 +65,103 @@ public class RoleServiceTests
 
         //Assert
         result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task GetUserRolesNamesAsync_ForNonExistingUser_ReturnsEmptyList()
+    {
+        //Arrange
+        UsersDatabaseContext usersDatabaseContext = await DatabaseHelper.CreateAndPrepareUsersDatabaseContextForTesting();
+        RoleService roleService = new(usersDatabaseContext);
+
+        //Act
+        List<string> result = await roleService.GetUserRolesNamesAsync(99999);
+
+        //Assert
+        result.Should().BeEmpty();
+    }
+
+    public static IEnumerable<object[]> GetUserRolesAsyncTestsData()
+    {
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 1 , UserId = 1},
+                new() { RoleId = 2 , UserId = 1},
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<GetUserRoleDto>()
+            {
+                new(1, "Admin", new DateTime(2023, 11, 19), 1),
+                new(2, "Moderator", new DateTime(2023, 11, 19), 1),
+                new(3, "User", new DateTime(2023, 11, 19), 1),
+            }
+        };
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 2 , UserId = 1},
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<GetUserRoleDto>()
+            {
+                new(2, "Moderator", new DateTime(2023, 11, 19), 1),
+                new(3, "User", new DateTime(2023, 11, 19), 1),
+            }
+        };
+        yield return new object[]
+        {
+            1,
+            new List<UserToRole>()
+            {
+                new() { RoleId = 3 , UserId = 1},
+            },
+            new List<GetUserRoleDto>()
+            {
+                new(3, "User", new DateTime(2023, 11, 19), 1),
+            }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetUserRolesAsyncTestsData))]
+    public async Task GetUserRolesAsync_ForExistingUser_ReturnsUserRolesNames(int userId, List<UserToRole> userToRoles, List<GetUserRoleDto> expected)
+    {
+        //Arrange
+        UsersDatabaseContext usersDatabaseContext = await DatabaseHelper.CreateAndPrepareUsersDatabaseContextForTesting();
+        RoleService roleService = new(usersDatabaseContext);
+        await usersDatabaseContext.Users.AddAsync(new User() { Password = "dasd321D!@#@!#a", Email = "test@test.com", Username = "name" });
+        await usersDatabaseContext.Roles.AddRangeAsync(new List<Role>()
+        {
+            new() { Name = "Admin", CreatedAt = new DateTime(2023, 11, 19) },
+            new() { Name = "Moderator", CreatedAt = new DateTime(2023, 11, 19) },
+            new() { Name = "User", CreatedAt = new DateTime(2023, 11, 19) },
+        });
+        await usersDatabaseContext.UsersToRoles.AddRangeAsync(userToRoles);
+        await usersDatabaseContext.SaveChangesAsync();
+
+        //Act
+        List<GetUserRoleDto> result = await roleService.GetUserRolesAsync(userId);
+
+        //Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async Task GetUserRolesAsync_ForNonExistingUser_ReturnsEmptyList()
+    {
+        //Arrange
+        UsersDatabaseContext usersDatabaseContext = await DatabaseHelper.CreateAndPrepareUsersDatabaseContextForTesting();
+        RoleService roleService = new(usersDatabaseContext);
+
+        //Act
+        List<GetUserRoleDto> result = await roleService.GetUserRolesAsync(99999);
+
+        //Assert
+        result.Should().BeEmpty();
     }
 }
