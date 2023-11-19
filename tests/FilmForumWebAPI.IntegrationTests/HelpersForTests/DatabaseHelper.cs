4877944b85f807f8a322386aa32698a9778193b5
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-11-19 01:55:06 +0100

    Refactor DatabaseHelper in integration tests

--- tests/FilmForumWebAPI.IntegrationTests/HelpersForTests/DatabaseHelper.cs
+++ tests/FilmForumWebAPI.IntegrationTests/HelpersForTests/DatabaseHelper.cs
@@ -5,13 +5,20 @@ namespace FilmForumWebAPI.IntegrationTests.HelpersForTests;
 
 public class DatabaseHelper
 {
+    private static readonly string _usersDatabaseConnectionString = "Server=localhost,2433;Database=UsersDb;Uid=SA;Pwd=zaq1@WSX;TrustServerCertificate=True";
+    
+    private static async Task ResetUsersDatabaseAsync(UsersDatabaseContext usersDatabaseContext)
+    {
+        await usersDatabaseContext.Database.EnsureDeletedAsync();
+        await usersDatabaseContext.Database.EnsureCreatedAsync();
+    }
+
     public static async Task<UsersDatabaseContext> CreateAndPrepareUsersDatabaseContextForTesting()
     {
         DbContextOptionsBuilder<UsersDatabaseContext> builder = new();
-        builder.UseSqlServer("Server=localhost,2433;Database=UsersDb;Uid=SA;Pwd=zaq1@WSX;TrustServerCertificate=True");
+        builder.UseSqlServer(_usersDatabaseConnectionString);
         UsersDatabaseContext usersDatabaseContext = new(builder.Options);
-        await usersDatabaseContext.Database.EnsureDeletedAsync();
-        await usersDatabaseContext.Database.EnsureCreatedAsync();
+        await ResetUsersDatabaseAsync(usersDatabaseContext);
         return usersDatabaseContext;
     }
 }
