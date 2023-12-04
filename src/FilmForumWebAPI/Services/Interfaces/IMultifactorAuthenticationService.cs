5d371613562b70a27b9d5223da8c2e7b3b5e7faa
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-04 10:52:23 +0100

    Simplify code

--- src/FilmForumWebAPI/Services/Interfaces/IMultifactorAuthenticationService.cs
+++ src/FilmForumWebAPI/Services/Interfaces/IMultifactorAuthenticationService.cs
@@ -3,6 +3,8 @@
 public interface IMultifactorAuthenticationService
 {
     Task<bool> VerifyCodeAsync(string email, string code);
+
     Task<string> GenerateUriAsync(string email);
+
     Task<byte[]> GenerateQRCodePNGAsync(string text);
 }
--- src/FilmForumWebAPI/Services/MultifactorAuthenticationService.cs
+++ src/FilmForumWebAPI/Services/MultifactorAuthenticationService.cs
@@ -1,47 +1,38 @@
-﻿using System.Text;
+﻿using FilmForumWebAPI.Services.Interfaces;
 using OtpNet;
 using QRCoder;
 using SimpleBase;
-using FilmForumWebAPI.Services.Interfaces;
+using System.Text;
 
 namespace FilmForumWebAPI.Services;
 
 public class MultifactorAuthenticationService : IMultifactorAuthenticationService
 {
-    private readonly QRCodeGenerator _qrCoderGenerator;
+    private readonly QRCodeGenerator _qrCoderGenerator = new();
 
-    public MultifactorAuthenticationService() 
-    {
-        _qrCoderGenerator = new QRCodeGenerator();
-    }
-
-    private string secret(string unique) => $"SECRETKEY{unique}";
+    private static string Secret(string unique) => $"SECRETKEY{unique}";
 
     public Task<bool> VerifyCodeAsync(string email, string code)
-    {
-        return Task.Run(() => {
-            byte[] secretBytes = Encoding.ASCII.GetBytes(secret(email));
-            var totp = new Totp(secretBytes, mode: OtpHashMode.Sha512);
+        => Task.Run(() =>
+                    {
+                        byte[] secretBytes = Encoding.ASCII.GetBytes(Secret(email));
+                        var totp = new Totp(secretBytes, mode: OtpHashMode.Sha512);
 
-            return totp.VerifyTotp(code, out _);
-        });
-    }
+                        return totp.VerifyTotp(code, out _);
+                    });
 
     public Task<string> GenerateUriAsync(string email)
-    {
-        return Task.Run(() => {
-            byte[] secretBytes = Encoding.ASCII.GetBytes(secret(email));
-            string base32 = Base32.Rfc4648.Encode(secretBytes);
-            return $"otpauth://totp/FilmForum:{email}?secret={base32}&issuer=FilmForum&algorithm=SHA512";
-        });
-    }
+        => Task.Run(() =>
+                    {
+                        byte[] secretBytes = Encoding.ASCII.GetBytes(Secret(email));
+                        string base32 = Base32.Rfc4648.Encode(secretBytes);
+                        return $"otpauth://totp/FilmForum:{email}?secret={base32}&issuer=FilmForum&algorithm=SHA512";
+                    });
 
-    public Task<byte[]> GenerateQRCodePNGAsync(string text) 
-    {
-        return Task.Run(() => {
-            QRCodeData qrData = _qrCoderGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
-            return new PngByteQRCode(qrData).GetGraphic(5);
-        });
-    }
-
+    public Task<byte[]> GenerateQRCodePNGAsync(string text)
+        => Task.Run(() =>
+                    {
+                        QRCodeData qrData = _qrCoderGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                        return new PngByteQRCode(qrData).GetGraphic(5);
+                    });
 }
