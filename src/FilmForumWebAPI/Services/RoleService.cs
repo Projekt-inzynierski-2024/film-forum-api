fcacf9b47c1c2ab1385f5d89dc9fc6fbfeb943e2
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-18 22:35:42 +0100

    Add comments in RoleService

--- src/FilmForumWebAPI/Services/RoleService.cs
+++ src/FilmForumWebAPI/Services/RoleService.cs
@@ -14,18 +14,35 @@ public class RoleService : IRoleService
 
     public RoleService(UsersDatabaseContext usersDatabaseContext) => _usersDatabaseContext = usersDatabaseContext;
 
+    /// <summary>
+    /// Gets names of all roles assigned to user
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <returns>Names of all roles assigned to user</returns>
     public async Task<List<string>> GetUserRolesNamesAsync(int userId)
         => await _usersDatabaseContext.Roles.Join(_usersDatabaseContext.UsersToRoles.Where(x => x.UserId == userId),
                                                  role => role.Id,
                                                  userToRole => userToRole.RoleId,
                                                  (role, userToRole) => role.Name).ToListAsync();
 
+    /// <summary>
+    /// Gets all roles assigned to user
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <returns>All roles assigned to user</returns>
     public async Task<List<GetUserRoleDto>> GetUserRolesAsync(int userId)
         => await _usersDatabaseContext.Roles.Join(_usersDatabaseContext.UsersToRoles.Where(x => x.UserId == userId),
                                                   role => role.Id,
                                                   userToRole => userToRole.RoleId,
                                                   (role, userToRole) => new GetUserRoleDto(role.Id, role.Name, role.CreatedAt, userToRole.UserId)).ToListAsync();
 
+    /// <summary>
+    /// Changes user's roles
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <param name="roles">New roles for user</param>
+    /// <returns>The total number of rows updated in the database</returns>
+    /// <exception cref="InvalidRoleNameException">When <paramref name="roles"/> has invalid value</exception>
     public async Task<int> ChangeUserRolesAsync(int userId, IEnumerable<UserRole> roles)
     {
         if (!await _usersDatabaseContext.Users.AnyAsync(x => x.Id == userId))
@@ -48,12 +65,17 @@ public class RoleService : IRoleService
         return await _usersDatabaseContext.SaveChangesAsync();
     }
 
-    public List<UserRole> PrepareUserRolesToSaveInDatabase(UserRole userMainRole)
-        => userMainRole switch
-        {
-            UserRole.Admin => new List<UserRole> { UserRole.Admin, UserRole.Moderator, UserRole.User },
-            UserRole.Moderator => new List<UserRole> { UserRole.Moderator, UserRole.User },
-            UserRole.User => new List<UserRole> { UserRole.User },
-            _ => throw new InvalidRoleNameException()
-        };
+    /// <summary>
+    /// Prepares user's roles to save in database returning proper list of roles to save
+    /// </summary>
+    /// <param name="userMainRole">Main role of user, because all subordinating roles needs to be attached</param>
+    /// <returns>List of proper roles for user that can be saved in database</returns>
+    /// <exception cref="InvalidRoleNameException">When <paramref name="userMainRole"/> has invalid value</exception>
+    public List<UserRole> PrepareUserRolesToSaveInDatabase(UserRole userMainRole) => userMainRole switch
+    {
+        UserRole.Admin => new List<UserRole> { UserRole.Admin, UserRole.Moderator, UserRole.User },
+        UserRole.Moderator => new List<UserRole> { UserRole.Moderator, UserRole.User },
+        UserRole.User => new List<UserRole> { UserRole.User },
+        _ => throw new InvalidRoleNameException()
+    };
 }
