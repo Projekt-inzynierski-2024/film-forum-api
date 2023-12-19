a44b78c35cb5cac5dbe226bae63f1f4f1d171d75
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-20 00:16:44 +0100

    Comments in ReviewService

--- src/FilmForumWebAPI/Services/ReviewService.cs
+++ src/FilmForumWebAPI/Services/ReviewService.cs
@@ -12,17 +12,44 @@ public class ReviewService : IReviewService
 
     public ReviewService(FilmsDatabaseContext filmsDatabaseContext) => _reviewCollection = filmsDatabaseContext.ReviewCollection;
 
+    /// <summary>
+    /// Adds new review to database
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <param name="createReviewDto">Details to create a review</param>
+    /// <returns>The result of the insert operation</returns>
     public async Task CreateAsync(string userId, CreateReviewDto createReviewDto) => await _reviewCollection.InsertOneAsync(new(userId, createReviewDto));
 
+    /// <summary>
+    /// Gets all reviews from database
+    /// </summary>
+    /// <returns>List of all reviews from database</returns>
     public async Task<List<GetReviewDto>> GetAllAsync()
         => await _reviewCollection.Find(_ => true).ToListAsync() is IEnumerable<Review> reviews ? reviews.Select(x => new GetReviewDto(x)).ToList() : new();
 
+    /// <summary>
+    /// Gets review with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Review's id</param>
+    /// <returns>Review with given <paramref name="id"/></returns>
     public async Task<GetReviewDto?> GetAsync(string id)
         => await _reviewCollection.Find(x => x.Id == id).FirstOrDefaultAsync() is Review review ? new(review) : null;
 
+    /// <summary>
+    /// Updates review with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Review's id</param>
+    /// <param name="userId">User's id</param>
+    /// <param name="createReviewDto">Details to update review</param>
+    /// <returns>The result of the replacement</returns>
     public async Task<ReplaceOneResult> UpdateAsync(string id, string userId, CreateReviewDto createReviewDto)
         => await _reviewCollection.ReplaceOneAsync(x => x.Id == id, new(id, userId, createReviewDto));
 
+    /// <summary>
+    /// Deletes review with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Review's id</param>
+    /// <returns>The result of the delete operation</returns>
     public async Task<DeleteResult> RemoveAsync(string id)
         => await _reviewCollection.DeleteOneAsync(x => x.Id == id);
 }
