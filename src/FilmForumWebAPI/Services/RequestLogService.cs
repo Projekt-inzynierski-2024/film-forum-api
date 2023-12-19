57c18132162639ef147777a87fd104e322e2dec8
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-19 21:02:18 +0100

    Comments in RequestLogService

--- src/FilmForumWebAPI/Services/RequestLogService.cs
+++ src/FilmForumWebAPI/Services/RequestLogService.cs
@@ -12,6 +12,11 @@ public class RequestLogService : IRequestLogService
 
     public RequestLogService(UsersDatabaseContext usersDatabaseContext) => _usersDatabaseContext = usersDatabaseContext;
 
+    /// <summary>
+    /// Saves new request log combined with user in database
+    /// </summary>
+    /// <param name="createRequestLogDto">Request's details to save</param>
+    /// <returns>The number of entities written to the database</returns>
     public async Task<int> CreateAsync(CreateRequestLogDto createRequestLogDto)
     {
         RequestLog requestLog = new(createRequestLogDto);
@@ -20,18 +25,42 @@ public class RequestLogService : IRequestLogService
         return await _usersDatabaseContext.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Gets all requests' logs from database
+    /// </summary>
+    /// <returns>List of requests' logs from database</returns>
     public async Task<List<GetRequestLogDto>> GetAllAsync()
         => await _usersDatabaseContext.RequestLogs.AsNoTracking().Select(x => new GetRequestLogDto(x)).ToListAsync();
 
+    /// <summary>
+    /// Gets request's log with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Request's log's id</param>
+    /// <returns>Request's log's details</returns>
     public async Task<GetRequestLogDto?> GetAsync(int id)
         => await _usersDatabaseContext.RequestLogs.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id) is RequestLog requestLog ? new GetRequestLogDto(requestLog) : null;
 
+    /// <summary>
+    /// Gets all requests' logs from database for user with given <paramref name="userId"/>
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <returns>List of all requests' logs for specified user</returns>
     public async Task<List<GetRequestLogDto>> GetUserAllRequestsLogsAsync(int userId)
         => await _usersDatabaseContext.RequestLogs.AsNoTracking().Where(x => x.UserId == userId).Select(x => new GetRequestLogDto(x)).ToListAsync();
 
+    /// <summary>
+    /// Removes request's log with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Request's log's id</param>
+    /// <returns>The number of rows deleted in the database</returns>
     public async Task<int> RemoveAsync(int id)
         => await _usersDatabaseContext.RequestLogs.Where(x => x.Id == id).ExecuteDeleteAsync();
 
+    /// <summary>
+    /// Removes all requests' logs from database for user with given <paramref name="userId"/>
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <returns>The number of rows deleted in the database</returns>
     public async Task<int> RemoveUserRequestsLogsAsync(int userId)
         => await _usersDatabaseContext.RequestLogs.Where(x => x.UserId == userId).ExecuteDeleteAsync();
 }
