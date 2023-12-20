a3d29d0fac619eb4deb18473388ea999dd01590c
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-20 23:48:18 +0100

    Comments in FilmService

--- src/FilmForumWebAPI/Services/FilmService.cs
+++ src/FilmForumWebAPI/Services/FilmService.cs
@@ -1,4 +1,5 @@
 ﻿using FilmForumModels.Dtos.FilmDtos;
+using FilmForumModels.Dtos.ReviewDtos;
 using FilmForumModels.Entities;
 using FilmForumWebAPI.Database;
 using FilmForumWebAPI.Services.Interfaces;
@@ -64,15 +65,35 @@ public class FilmService : IFilmService
     public async Task<List<GetFilmDto>> GetAllAsync()
         => await _filmCollection.Find(_ => true).ToListAsync() is IEnumerable<Film> films ? films.Select(x => new GetFilmDto(x)).ToList() : new();
 
+    /// <summary>
+    /// Returns list of all film with details from database
+    /// </summary>
+    /// <returns>List of all film with details from database</returns>
     public async Task<List<GetDetailedFilmDto>> GetDetailedAllAsync()
        => await _detailedFilmCursor.ToListAsync() is IEnumerable<Film> films ? films.Select(x => new GetDetailedFilmDto(x)).ToList() : new();
 
+    /// <summary>
+    /// Adds new film to database
+    /// </summary>
+    /// <param name="createFilmDto">Details to create new film</param>
+    /// <returns>The result of the insert operation</returns>
     public async Task CreateAsync(CreateFilmDto createFilmDto)
         => await _filmCollection.InsertOneAsync(new(createFilmDto));
 
+    /// <summary>
+    /// Updates film with given <paramref name="id"/> in database
+    /// </summary>
+    /// <param name="id">Film's id</param>
+    /// <param name="updatedFilm">Details to update film</param>
+    /// <returns>The result of the replacement</returns>
     public async Task<ReplaceOneResult> UpdateAsync(string id, CreateFilmDto updatedFilm)
         => await _filmCollection.ReplaceOneAsync(x => x.Id == id, new(id, updatedFilm));
 
+    /// <summary>
+    /// Deletes film with given <paramref name="id"/> from database
+    /// </summary>
+    /// <param name="id">Film's id</param>
+    /// <returns>The result of the delete operation</returns>
     public async Task<DeleteResult> RemoveAsync(string id)
         => await _filmCollection.DeleteOneAsync(x => x.Id == id);
 }
