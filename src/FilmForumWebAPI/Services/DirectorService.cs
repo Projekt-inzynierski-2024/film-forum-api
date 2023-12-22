c1b7c0ad17121e05d62e50d1728a26e420133680
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-22 16:10:23 +0100

    Comments in DirectorService

--- src/FilmForumWebAPI/Services/DirectorService.cs
+++ src/FilmForumWebAPI/Services/DirectorService.cs
@@ -12,11 +12,25 @@ public class DirectorService : IDirectorService
 
     public DirectorService(FilmsDatabaseContext filmsDatabaseContext) => _directorCollection = filmsDatabaseContext.DirectorCollection;
 
+    /// <summary>
+    /// Returns list of directors whose name or surname matches <paramref name="query"/> from database
+    /// </summary>
+    /// <param name="query">Director's name or surname</param>
+    /// <returns>List of directors with matching names or surnames</returns>
     public async Task<List<GetDirectorDto>> SearchAllAsync(string query)
         => await _directorCollection.Find(x => (x.Name + " " + x.Surname).ToUpper().Contains(query.ToUpper())).ToListAsync() is IEnumerable<Director> list ? list.Select(x => new GetDirectorDto(x)).ToList() : new();
 
+    /// <summary>
+    /// Adds new director to database
+    /// </summary>
+    /// <param name="createDirectorDto">Details to create new director</param>
+    /// <returns>The result of the insert operation</returns>
     public async Task CreateAsync(CreateDirectorDto createDirectorDto) => await _directorCollection.InsertOneAsync(new(createDirectorDto));
 
+    /// <summary>
+    /// Returns list of all directors from database
+    /// </summary>
+    /// <returns>List of all directors from database</returns>
     public async Task<List<GetDirectorDto>> GetAllAsync()
         => await _directorCollection.Find(_ => true).ToListAsync() is IEnumerable<Director> directors ? directors.Select(x => new GetDirectorDto(x)).ToList() : new();
 
