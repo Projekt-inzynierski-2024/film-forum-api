199361444266e288ffd2f6629ad17a9ae83a92c8
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-28 01:14:03 +0100

    Comments in ActorService

--- src/FilmForumWebAPI/Services/ActorService.cs
+++ src/FilmForumWebAPI/Services/ActorService.cs
@@ -12,11 +12,25 @@ public class ActorService : IActorService
 
     public ActorService(FilmsDatabaseContext filmsDatabaseContext) => _actorCollection = filmsDatabaseContext.ActorCollection;
 
+    /// <summary>
+    /// Returns list of actors whose name or surname matches <paramref name="query"/> from database
+    /// </summary>
+    /// <param name="query">Actor's name or surname</param>
+    /// <returns>List of actors with matchind name or surname</returns>
     public async Task<List<GetActorDto>> SearchAllAsync(string query)
         => await _actorCollection.Find(x => (x.Name + " " + x.Surname).ToUpper().Contains(query.ToUpper())).ToListAsync() is IEnumerable<Actor> list ? list.Select(x => new GetActorDto(x)).ToList() : new();
 
+    /// <summary>
+    /// Adds new actor to database
+    /// </summary>
+    /// <param name="createActorDto">Details to create actor</param>
+    /// <returns>The result of the insert operation</returns>
     public async Task CreateAsync(CreateActorDto createActorDto) => await _actorCollection.InsertOneAsync(new(createActorDto));
 
+    /// <summary>
+    /// Returns list of all actors from database
+    /// </summary>
+    /// <returns>List of all actors</returns>
     public async Task<List<GetActorDto>> GetAllAsync()
         => await _actorCollection.Find(_ => true).ToListAsync() is IEnumerable<Actor> list ? list.Select(x => new GetActorDto(x)).ToList() : new();
 
